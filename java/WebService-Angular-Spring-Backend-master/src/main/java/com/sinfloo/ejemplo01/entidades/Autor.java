99e021301886a8c2b1d854e575f93376878e8b88
damian <adamodamian6@gmail.com>
2022-11-27 15:27:07 -0300

    primer commit

---
+++ java/WebService-Angular-Spring-Backend-master/src/main/java/com/sinfloo/ejemplo01/entidades/Autor.java
@@ -0,0 +1,39 @@
+
+package com.sinfloo.ejemplo01.entidades;
+
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+
+public class Autor {
+    
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid",strategy="uuid2")
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) genera valor numerico
+    private String id;
+
+    private String name;
+  
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    
+    
+}
